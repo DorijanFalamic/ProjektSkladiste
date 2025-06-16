2aefaf457a3a36eef1c65b981397d841afb5a6b4
DorijanFalamic <dorijan.dorijan84@gmail.com>
2025-06-16 22:08:52 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,33 +1,33 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS // Isključivanje upozorenja vezanih za sigurnost CRT funkcija
 #ifndef HEADER_H
 #define HEADER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h> // Standardni ulaz/izlaz
+#include <stdlib.h> // Standardna biblioteka, koristi se za malloc i free
+#include <string.h> // Za rad sa stringovima
 
-#define MAX_NAZIV 50
-#define MAX_KATEGORIJA 50
-#define MAX_PROIZVODI 100
+#define MAX_NAZIV 50 // Maksimalna duljina naziva proizvoda
+#define MAX_KATEGORIJA 50 // Maksimalna duljina naziva kategorije (trenutno nedefinirano u funkcijama)
+#define MAX_PROIZVODI 100 // Maksimalan broj proizvoda u skladištu
 
-typedef enum { VOCE, POVRCE } Kategorija; // stavka 11 sa obaveznih koncepta
+typedef enum { VOCE, POVRCE } Kategorija; // Definicija tipizirane varijable za kategorije stavka 11 sa obaveznih koncepata
 
-typedef struct {
-    int id;
-    char naziv[MAX_NAZIV];
-    Kategorija kategorija;
-    float kolicina;
-    float cijena;
+typedef struct { // Struktura za proizvod
+    int id; // Jedinstveni identifikator proizvoda
+    char naziv[MAX_NAZIV]; // Naziv proizvoda
+    Kategorija kategorija; // Kategorija proizvoda (voće ili povrće)
+    float kolicina; // Količina proizvoda u kilogramima
+    float cijena; // Cijena po kilogramu
 } Proizvod;
 
-typedef struct {
-    Proizvod* niz;
-    size_t brojProizvoda;
+typedef struct { // Struktura skladišta
+    Proizvod* niz; // Pokazivač na niz proizvoda u memoriji
+    size_t brojProizvoda; // Broj trenutno pohranjenih proizvoda
 } Skladiste;
 
-extern Skladiste skladiste; // stavka 8 sa obaveznih koncepta
+extern Skladiste skladiste; // Deklaracija globalne varijable skladiste za korištenje u drugim datotekama
 
-// Funkcije
+// Deklaracije funkcija koje se koriste u programu
 void dodajProizvod();
 void prikaziProizvode();
 void pretraziProizvod();
