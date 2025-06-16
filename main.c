a07c776975720dbedf826891592f11c89a823561
DorijanFalamic <dorijan.dorijan84@gmail.com>
2025-06-16 21:45:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include "header.h"
 
-int main() {
-    int izbor;
 
+
+
+int main() {
+    ucitajProizvode(); // Učitavanje u memoriju
+
+    int izbor;
     do {
         printf("\n--- Skladiste voca i povrca ---\n");
         printf("1. Dodaj novi proizvod\n");
@@ -12,34 +16,30 @@ int main() {
         printf("3. Pretrazi proizvod\n");
         printf("4. Obrisi proizvod\n");
         printf("5. Uredi proizvod\n");
-        printf("6. Izlaz\n");
+        printf("6. Sortiraj proizvode\n");
+        printf("7. Izlaz\n");
 
         printf("Odabir: ");
         scanf("%d", &izbor);
 
         switch (izbor) {
-        case 1:
-            dodajProizvod();
-            break;
-        case 2:
-            prikaziProizvode();
-            break;
-        case 3:
-            pretraziProizvod();
-            break;
-        case 4:
-            obrisiProizvod();
-            break;
-        case 5: 
-            UrediProizvod();
-            break;
+        case 1: dodajProizvod(); break;
+        case 2: prikaziProizvode(); break;
+        case 3: pretraziProizvod(); break;
+        case 4: obrisiProizvod(); break;
+        case 5: urediProizvod(); break;
         case 6:
-            printf("Izlaz iz programa.\n");
+            sortirajProizvode();
+            sacuvajProizvode();  // Spremi sortirane podatke u datoteku
+            printf("Sortirano i spremljeno u datoteku.\n");
             break;
-        default:
-            printf("Nepoznata opcija. Pokusajte ponovno.\n");
+        case 7: printf("Izlaz iz programa.\n"); break;
+        default: printf("Nepoznata opcija.\n");
         }
-    } while (izbor != 6); 
 
+    } while (izbor != 7);
+
+    sacuvajProizvode(); // spremi na kraju
+    free(skladiste.niz); // oslobodi memoriju - stavka 18
     return 0;
 }
