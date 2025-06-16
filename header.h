45d15bd1ca59bf6fce4fc5456ee49f92dc916c23
DorijanFalamic <dorijan.dorijan84@gmail.com>
2025-06-16 21:44:25 +0200

    Update header.h

--- header.h
+++ header.h
@@ -2,22 +2,40 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define MAX_NAZIV 50
-#define MAX_KATEGORIJA 30
+#define MAX_KATEGORIJA 50
+#define MAX_PROIZVODI 100
+
+typedef enum { VOCE, POVRCE } Kategorija; // stavka 11 sa obaveznih koncepta
 
 typedef struct {
-	int id;
-	char naziv[MAX_NAZIV];
-	char kategorija[MAX_KATEGORIJA];
-	float kolicina;
-	float cijena;
+    int id;
+    char naziv[MAX_NAZIV];
+    Kategorija kategorija;
+    float kolicina;
+    float cijena;
 } Proizvod;
+
+typedef struct {
+    Proizvod* niz;
+    size_t brojProizvoda;
+} Skladiste;
+
+extern Skladiste skladiste; // stavka 8 sa obaveznih koncepta
 
 // Funkcije
 void dodajProizvod();
 void prikaziProizvode();
 void pretraziProizvod();
 void obrisiProizvod();
-void UrediProizvod(); 
+void urediProizvod();
+void ucitajProizvode();
+void sacuvajProizvode();
+void sortirajProizvode();
+void* binarnaPretraga(const void* kljuc, const void* element);
 
 #endif
