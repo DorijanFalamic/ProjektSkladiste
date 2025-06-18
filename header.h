f27361bd3889fed229ebaa0e43185e04d8ddae1b
DorijanFalamic <dorijan.dorijan84@gmail.com>
2025-06-18 22:14:11 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,41 +1,69 @@
-#define _CRT_SECURE_NO_WARNINGS // Isključivanje upozorenja vezanih za sigurnost CRT funkcija
+#define _CRT_SECURE_NO_WARNINGS
+
 #ifndef HEADER_H
 #define HEADER_H
 
-#include <stdio.h> // Standardni ulaz/izlaz
-#include <stdlib.h> // Standardna biblioteka, koristi se za malloc i free
-#include <string.h> // Za rad sa stringovima
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAX_NAZIV 50 // Maksimalna duljina naziva proizvoda
-#define MAX_KATEGORIJA 50 // Maksimalna duljina naziva kategorije (trenutno nedefinirano u funkcijama)
-#define MAX_PROIZVODI 100 // Maksimalan broj proizvoda u skladištu
+#define MAX_NAZIV 50
+#define MAX_KATEGORIJA 50
+#define MAX_PROIZVODI 100
 
-typedef enum { VOCE, POVRCE } Kategorija; // Definicija tipizirane varijable za kategorije stavka 11 sa obaveznih koncepata
+// Makro funkcija (koncept 9)
+#define MAKS(a, b) ((a) > (b) ? (a) : (b))
 
-typedef struct { // Struktura za proizvod
-    int id; // Jedinstveni identifikator proizvoda
-    char naziv[MAX_NAZIV]; // Naziv proizvoda
-    Kategorija kategorija; // Kategorija proizvoda (voće ili povrće)
-    float kolicina; // Količina proizvoda u kilogramima
-    float cijena; // Cijena po kilogramu
+// Inline funkcija (koncept 9)
+static inline float minFloat(float a, float b) { return a < b ? a : b; }
+
+// Enum za kategoriju proizvoda
+typedef enum {
+    VOCE,
+    POVRCE
+} Kategorija;
+
+// Enum za izbornik (koncept 11)
+typedef enum {
+    DODAJ = 1,
+    PRIKAZI,
+    PRETRAZI,
+    OBRISI,
+    UREDI,
+    SORTIRAJ,
+    VELICINA,
+    IZLAZ
+} IzbornikOpcije;
+
+typedef struct {
+    int id;
+    char naziv[MAX_NAZIV];
+    Kategorija kategorija;
+    float kolicina;
+    float cijena;
 } Proizvod;
 
-typedef struct { // Struktura skladišta
-    Proizvod* niz; // Pokazivač na niz proizvoda u memoriji
-    size_t brojProizvoda; // Broj trenutno pohranjenih proizvoda
+typedef struct {
+    Proizvod* niz;
+    size_t brojProizvoda;
 } Skladiste;
 
-extern Skladiste skladiste; // Deklaracija globalne varijable skladiste za korištenje u drugim datotekama
+extern Skladiste skladiste;
 
-// Deklaracije funkcija koje se koriste u programu
+// Deklaracije funkcija
 void dodajProizvod();
 void prikaziProizvode();
 void pretraziProizvod();
+void pretraziProizvodPoNazivu();
+void pretraziProizvodBsearch();
 void obrisiProizvod();
 void urediProizvod();
 void ucitajProizvode();
 void sacuvajProizvode();
 void sortirajProizvode();
-void* binarnaPretraga(const void* kljuc, const void* element);
+void prikaziVelicinuDatoteke();
+int usporediProizvodId(const void* a, const void* b);
+void clearScreen();
+int postojiId(int id);
 
 #endif
